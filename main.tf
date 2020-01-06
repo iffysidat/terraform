33ca5f77b927092aa22f288d0e7264b8c91df1ff
Irfan Sidat <irfan.sidat@bjss.com>
2020-01-06 15:41:49 +0000

    Added security groups and a variable and an output variable

--- main.tf
+++ main.tf
@@ -3,10 +3,39 @@ provider "aws" {
 }
 
 resource "aws_instance" "myfirstinstance" {
-    ami           = "ami-05f37c3995fffb4fd"
-    instance_type = "t2.micro"
+    ami                    = "ami-0be057a22c63962cb"
+    instance_type          = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.myfirstsecuritygroup.id]
+
+    user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
 
     tags = {
         Name = "irfan-first-instance"
     }
+}
+
+resource "aws_security_group" "myfirstsecuritygroup" {
+    name = "irfan-first-security-group"
+
+    ingress {
+        from_port   = var.server_port
+        to_port     = var.server_port
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+variable "server_port" {
+    description = "The port the server will use for HTTP requests"
+    type        = number
+    default     = 8080
+}
+
+output "public_ip" {
+    value       = aws_instance.myfirstinstance.public_ip
+    description = "The public IP address of the web server"
 }
