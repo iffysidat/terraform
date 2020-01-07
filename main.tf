6c8bdb0d47c63d2641ba474e388ae9a5065e2e3c
Irfan Sidat <irfan.sidat@bjss.com>
2020-01-07 14:28:22 +0000

    Added configuration for an alb and another security group

--- main.tf
+++ main.tf
@@ -2,6 +2,14 @@ provider "aws" {
     region = "eu-west-2"
 }
 
+data "aws_vpc" "myfirstvpc" {
+    default = true
+}
+
+data "aws_subnet_ids" "myfirstsubnetids" {
+    vpc_id = data.aws_vpc.myfirstvpc.id
+}
+
 resource "aws_instance" "myfirstinstance" {
     ami                    = "ami-0be057a22c63962cb"
     instance_type          = "t2.micro"
@@ -9,7 +17,7 @@ resource "aws_instance" "myfirstinstance" {
 
     user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, World" > index.html
+              echo "Hello, my name is Irfan" > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
@@ -29,13 +37,122 @@ resource "aws_security_group" "myfirstsecuritygroup" {
     }
 }
 
+resource "aws_security_group" "albsg" {
+    name = "irfan-alb-securitygroup"
+
+    # Allow inbound HTTP requests
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    # Allow all outbound requests
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_launch_configuration" "myfirstlaunchconfig" {
+    image_id        = "ami-0be057a22c63962cb"
+    instance_type   = "t2.micro"
+    security_groups = [aws_security_group.myfirstsecuritygroup.id]
+
+    user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, my name is Irfan" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+
+resource "aws_autoscaling_group" "myfirstasg" {
+    launch_configuration    = aws_launch_configuration.myfirstlaunchconfig.name
+    vpc_zone_identifier     = data.aws_subnet_ids.myfirstsubnetids.ids
+
+    target_group_arns       = [aws_lb_target_group.myfirstasgtargetgroup.arn]
+    health_check_type       = "ELB"
+
+    min_size = 2
+    max_size = 10
+
+    tag {
+        key                 = "Name"
+        value               = "irfan-first-asg-example"
+        propagate_at_launch = true
+    }
+}
+
+resource "aws_lb" "myfirstlb" {
+    name               = "irfan-first-loadbalancer"
+    load_balancer_type = "application"
+    subnets            = data.aws_subnet_ids.myfirstsubnetids.ids
+    security_groups    = [aws_security_group.albsg.id]
+}
+
+resource "aws_lb_listener" "myfirstlistener" {
+    load_balancer_arn = aws_lb.myfirstlb.arn
+    port              = 80
+    protocol          = "HTTP"
+
+    # By default, return a simple 404 page
+    default_action {
+        type = "fixed-response"
+
+        fixed_response {
+            content_type = "text/plain"
+            message_body = "404: page not found"
+            status_code  = 404
+        }
+    }
+}
+
+resource "aws_lb_target_group" "myfirstasgtargetgroup" {
+    name     = "irfan-first-asgtargetgroup"
+    port     = var.server_port
+    protocol = "HTTP"
+    vpc_id   = data.aws_vpc.myfirstvpc.id
+
+    health_check {
+        path                = "/"
+        protocol            = "HTTP"
+        matcher             = "200"
+        interval            = 15
+        timeout             = 3
+        healthy_threshold   = 2
+        unhealthy_threshold = 2
+    }
+}
+
+resource "aws_lb_listener_rule" "myfirstlblistenerrule" {
+    listener_arn = aws_lb_listener.myfirstlistener.arn
+    priority     = 100
+
+    condition {
+        field  = "path-pattern"
+        values = ["*"] 
+    }
+
+    action {
+        type             = "forward"
+        target_group_arn = aws_lb_target_group.myfirstasgtargetgroup.arn 
+    }
+}
+
 variable "server_port" {
     description = "The port the server will use for HTTP requests"
     type        = number
     default     = 8080
 }
 
-output "public_ip" {
-    value       = aws_instance.myfirstinstance.public_ip
-    description = "The public IP address of the web server"
+output "dns_alb_name" {
+    value       = aws_lb.myfirstlb.dns_name
+    description = "The domain name of the load balancer"
 }
